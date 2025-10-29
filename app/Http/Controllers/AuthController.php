e2dab9480421c79c00a815e8e5df81cb3b90bdd4
Bruno Antunes <brunoantunes039@gmail.com>
2025-10-29 16:49:17 -0300

    introdução a validação de dados

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,5 +13,13 @@ class AuthController extends Controller
     public function logout(){
         echo 'logout';
     }
+    public function loginSubmit(Request $request){
+        echo 'login submit '.$request->input('text_username');
+        echo '<br>';
+        echo  'password submit'.$request->input('text_password');
+        echo '<br>';
+        dd($request->all());
+
+    }
 
 }
