d9260a692370a5ab01778cacf97d538d43418cf5
Bruno Antunes <brunoantunes039@gmail.com>
2025-11-10 20:04:52 -0300

    criacão da função decrypt no MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 class MainController extends Controller
 {
@@ -24,12 +26,34 @@ class MainController extends Controller
 
     public function newNote()
     {
-        echo"<h1>criando uma nova anotação</h1>";
+        echo "<h1>criando uma nova anotação</h1>";
     }
 
     public function editNote($id)
-    {
-        $decryptedId = decrypt($id);
-        echo"<h1>Editando a anotação de ID: ".$decryptedId."</h1>";
+    {   //decrypt id e tratamento de erro
+        $id = $this->decryptId($id);
+        echo "Editando anotação de id: $id";
+
+
     }
+
+       public function deleteNote($id)
+    {   //delete note id e tratamento de erro
+        $id=$this->decryptId($id);
+        echo "<h1>Deletando anotação de id= :$id<h1>";
+
+
+    }
+
+    private function decryptId($id)
+    {   //check if $id is encrypted or not
+          try {
+            $decrypted_id = Crypt::decrypt($id);
+
+        } catch (DecryptException $e) {
+            // Decryption failed — handle appropriately (e.g., show 404 or error message)
+            return redirect()->route('home');
+        }
+    }
+
 }
